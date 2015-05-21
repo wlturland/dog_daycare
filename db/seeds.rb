3cb31b38a736ff64c12f61ee592f5c9b697588d5
Leigh Turland <wlturland@gmail.com>
2015-05-21 09:10:51 -0500

    db/seeds creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Breed.create(breed: 'Australian Shepherd')
+Breed.create(breed: 'Border Collie')
+Breed.create(breed: 'Jack Russell')
+Breed.create(breed: 'Pit Bull')
+Breed.create(breed: 'Shih-tzu')
+Breed.create(breed: 'Standard Poodle')
+Breed.create(breed: 'Miniature Poodle')
+Breed.create(breed: 'Doberman')
+Breed.create(breed: 'Bulldog')
+Breed.create(breed: 'Malamute')
+Breed.create(breed: 'Eskimo')
+Breed.create(breed: 'Chihuahua')
+Breed.create(breed: 'Yorkie')
+Breed.create(breed: 'Boxer')
