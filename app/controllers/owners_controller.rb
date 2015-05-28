fee2d48f2999bc15725ba15c137e6d26055e3ea9
Leigh Turland <wlturland@gmail.com>
2015-05-28 10:55:19 -0500

    Updated Controllers and Views for User Authentications

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,5 +1,6 @@
 class OwnersController < ApplicationController
   before_action :set_owner, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /owners
   # GET /owners.json
