ef861fe6adac0b61d288ca0ac428f5ad5cbca36f
Leigh Turland <wlturland@gmail.com>
2015-05-28 10:04:25 -0500

    Devise User Model and Migration

--- test/models/breed_test.rb
+++ test/models/breed_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: breeds
+#
+#  id         :integer          not null, primary key
+#  breed      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class BreedTest < ActiveSupport::TestCase
--- test/models/dog_test.rb
+++ test/models/dog_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: dogs
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  owner_id            :integer
+#  breed_id            :integer
+#  dob                 :date
+#  vet                 :string
+#  vet_phone           :string
+#  in_daycare          :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+
 require 'test_helper'
 
 class DogTest < ActiveSupport::TestCase
